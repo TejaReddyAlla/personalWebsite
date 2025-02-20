d6256b68974bfd11c5f5ef26cbc3b443a38a6635
Teja Reddy Alla <tejareddyalla@Tejas-MacBook-Pro.local>
2025-02-19 16:31:03 -0800

    testimonials section - setup

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import CheckCircleIcon from '@/assets/icons/check-circle.svg';
 import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 import grainImage from '@/assets/images/grain.jpg'
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/card";
 
 
 const portfolioProjects = [
@@ -49,26 +51,12 @@ const portfolioProjects = [
 export const ProjectsSection = () => {
   return <section className="pb-16 lg:py-24">
     <div className="container">
-      <div className="flex justify-center">
-        <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">
-          Real-world results
-        </p>
-      </div>
-      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6"> Featured Projects</h2>
-      <p className="text-center md:text-large lg:text-xl text-white/60 mt-4 max-w-md mx-auto"> See how I transformed concepts into engaing digital experiences</p>
-      <div className="flex md:mt-20 flex-col mt-10 gap-20">
+      <SectionHeader eyebrow="Real-world results" title="Featured Projects"
+        description="See how I transformed concepts into engaing digital experiences"
+      />
+      <div className="mt-10 flex md:mt-20 flex-col  gap-20">
         {portfolioProjects.map(project => (
-          <div key={project.title} className="bg-gray-800 rounded-3xl 
-               relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 
-               after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl
-               ater:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 
-               after:pointer-events-none">
-
-            <div className="absolute inset-0 -z-10 opacity-5" style={{
-              backgroundImage: `url(${grainImage.src})`,
-            }}>
-
-            </div>
+          <Card key={project.title} className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20 ">
 
             <div className="lg:grid lg:grid-cols-2 lg:gap-16">
               <div className="lg:pb-16">
@@ -109,7 +97,7 @@ export const ProjectsSection = () => {
               </div>
             </div>
 
-          </div>
+          </Card>
         ))}
       </div>
     </div>
